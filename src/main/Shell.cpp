7f0e99c597c4003e2a784a756ed44de4973bc9b5
apalermooo <ajp2s@missouristate.edu>
2026-03-06 19:39:51 -0600

    build tools implemented

--- src/main/Shell.cpp
+++ src/main/Shell.cpp
@@ -2,7 +2,14 @@
 // Created by Cinnamon on 3/6/2026.
 //
 
-#include "Shell.h"
+#include "include/Shell.h"
+
+#include <iostream>
+#include <ostream>
 
 // This file will contain the implementation of the CLI environment
 // It will handle taking commands from the user, and executing the corresponding
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+}
