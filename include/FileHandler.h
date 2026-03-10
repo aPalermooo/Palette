7f7b0d93426abf42970cad5426dbefaee36c2a96
apalermooo <ajp2s@missouristate.edu>
2026-03-09 19:35:44 -0500

    Refactor scaffolding (redo cmake config)

--- include/FileHandler.h
+++ include/FileHandler.h
@@ -9,7 +9,8 @@
 
 #ifndef PALETTE_FILEHANDLER_H
 #define PALETTE_FILEHANDLER_H
-#include "src/gtest-internal-inl.h"
+#include <filesystem>
+#include <vector>
 
 class FileHandler {
 private:
@@ -23,18 +24,6 @@ public:
     static std::vector<std::filesystem::directory_entry> getDirectory(const std::filesystem::path& path);
 
 };
-
-inline void FileHandler::openFile(const std::filesystem::path& path) {
-    ShellExecuteA(nullptr, "open", path.string().c_str(), nullptr, nullptr, SW_SHOWDEFAULT);
-}
-
-inline std::vector<std::filesystem::directory_entry> FileHandler::getDirectory(const std::filesystem::path& path) {
-    std::vector<std::filesystem::directory_entry> entries;
-    for (const auto & entry : std::filesystem::directory_iterator(path)) {
-        entries.push_back(entry);
-    }
-    return entries;
-}
 
 
 #endif //PALETTE_FILEHANDLER_H
