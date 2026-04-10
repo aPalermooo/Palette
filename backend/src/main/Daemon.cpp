2f55922a3c2fcc466a2aa7602737aadf9515cb61
apalermooo <ajp2s@missouristate.edu>
2026-04-09 21:03:33 -0500

    Tray Icon Implementation

--- backend/src/main/Daemon.cpp
+++ backend/src/main/Daemon.cpp
@@ -8,56 +8,20 @@
  * Course:          CSC450
  ***********************************/
 
+#define _WIN32_WINNT 0x0A00
 
 #include <iostream>
 #include <ostream>
 #include "crow.h"
 
-#include "database/Database.h"
 
-static std::filesystem::path dbPath;
-static std::filesystem::path filePath;
-static std::unique_ptr<Database> db;
+#include "TrayIcon.h"
 
-/**
- * @brief Runs at the start of application; prepares environment for runtime
- *
- * @note In case of runtime failure, call StartUp to refresh
- */
-void StartUp () {
-
-    // Find File Structure and Database Locations
-    filePath = FILE_PATH;
-    if (filePath.empty()) {
-        // Locate/Set to proper location
-    }
-    dbPath = DB_PATH;
-    if (dbPath.empty()) {
-        // Locate/Set to proper location
-    }
-
-    // Establish Database connection
-    db = std::make_unique<Database>(dbPath);
-}
 
 /**
  * @brief Listener and Hooks
  */
-void BackgroundLoop ();
-
-/**
- * @brief Safely exit program; safe all data
- */
-void ShutDown () {
-    db.reset();
-}
-
-
-int main() {
-
-    std::cout << "ENVIRONMENT: " << std::endl;
-    std::cout << "\tFile Structure Path: " << FILE_PATH << std::endl;
-    std::cout << "\tDatabase Path: " << DB_PATH << std::endl;
+void backgroundLoop () {
 
     crow::SimpleApp app;
 
@@ -67,7 +31,20 @@ int main() {
 
     app.port(18080).multithreaded().run();
 
-    // Start Up
+}
+
+
+
+int main() {
+
+    std::cout << "ENVIRONMENT: " << std::endl;
+    std::cout << "\tFile Structure Path: " << FILE_PATH << std::endl;
+    std::cout << "\tDatabase Path: " << DB_PATH << std::endl;
+
+
+    //Start Up
+    TrayIcon trayIcon{HINSTANCE()};
+    trayIcon.run();
 
     // Branch Background Process
     // Runtime Loop
